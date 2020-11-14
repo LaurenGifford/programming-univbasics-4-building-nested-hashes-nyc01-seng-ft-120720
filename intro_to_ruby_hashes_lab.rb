aff48075cabb84534141f9a35b4e87ec25146e6d
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-14 19:50:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,7 @@ def monopoly_with_second_tier
 hash = {
   :railroads => {}
 }
-pieces = {:pieces => 4}
-hash[:railraods] << pieces
+hash[:railraods][:pieces] = "4"
 rent = {:rent_in_dollars => {}}
 hash[:railroads] << rent
 names = {:names => {}}
